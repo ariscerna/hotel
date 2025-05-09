d1c6a20f59ade4ab2e2f21468cf4f0412ff3085a
ariscerna <ariscerna504@gmail.com>
2025-05-09 10:52:07 -0600

    craeción del  proyecto
    Iniciamos con  el diseño  del proyecto

---
+++ Hotel/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel
+{
+    public partial class FrmSistema : Form
+    {
+        public FrmSistema()
+        {
+            InitializeComponent();
+        }
+
+    }
+       
+}
